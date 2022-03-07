17fed3fcda058176514e72a426d7553d7f65be23
yoon20 <78343903+yoon20@users.noreply.github.com>
2022-03-07 22:38:23 +0900

    Add files via upload

---
+++ TodoItem.js
@@ -0,0 +1,20 @@
+import React,{ useState } from 'react';
+import { Button, Switch, Text } from 'react-native';
+import Row from './Row';
+ 
+function TodoItem({ label, id, onDelete }){
+  const [ isDone, setDone ] = useState( false );
+    return (
+        <Row style={{ alignItems: 'center', marginBottom: 12, }}>
+          <Switch value={ isDone }
+                  onValueChange={ value => setDone( value )}
+                  style={{ marginRight: 8, }}  
+                  />
+          <Text style={{ color: isDone ? '#eee' : '#000', flex: 1 }}> 
+          { label ?? '(내용 없음)'}
+          </Text>
+          <Button title="삭제" onPress={ ()=> onDelete( id )} />
+        </Row>
+    )
+}
+export default TodoItem;    
